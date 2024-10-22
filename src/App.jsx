76fc7f61cbb7228db662dafecf65a91d04709e3d
Gourav Chaudhary <gouravsittam@gmail.com>
2024-10-23 00:14:18 +0530

    Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline bg-blue-500 text-green-600">
-      Hello world!
-    </h1>
-  )
-}
+import React from "react";
+import Body from "./Components/Body";
+
+const App = () => {
+  return <Body />;
+};
+
+export default App;
