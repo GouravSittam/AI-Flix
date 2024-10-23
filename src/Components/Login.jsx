c10f422e199afee7b75843a6a35befc16ef0670c
Gourav Chaudhary <gouravsittam@gmail.com>
2024-10-23 12:21:19 +0530

    Code

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
           src="https://assets.nflxext.com/ffe/siteui/vlv3/7c0e18aa-2c95-474d-802e-7f30e75dcca4/web/IN-en-20241014-TRIFECTA-perspective_e7121311-c11e-4809-a3e6-22abffa33569_large.jpg"
           alt="logo"
         />
-      </div> 
+      </div>
     </div>
   );
 };
